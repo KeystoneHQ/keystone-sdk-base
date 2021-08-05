cc0f84d642468c212128cb455f66c83f4a606fb7
aaronisme <aarondongchen@gmail.com>
2021-08-05 17:36:02 +0800

    add the loadingspinner

---
+++ packages/sdk/src/components/LoadingSpinner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const LoadingSpinner = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ margin: 'auto', display: 'block', shapeRendering: 'auto', animationPlayState: 'running', animationDelay: '0s' }}
+            width="200px"
+            height="200px"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid">
+            <circle cx="50" cy="50" fill="none" stroke="#9b9b9b" stroke-width="7" r="35" stroke-dasharray="164.93361431346415 56.97787143782138"
+                style={{ animationPlayState: 'running', animationDelay: '0s' }}>
+                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"
+                    style={{ animationPlayState: 'running', animationDelay: '0s' }}>
+                </animateTransform>
+            </circle>
+        </svg>
+    );
+};
