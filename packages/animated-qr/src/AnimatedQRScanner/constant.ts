a82e81ee41d049222a40e304751438afb4d53395
yan.li <liyan1924@gmail.com>
2022-04-20 16:17:50 +0800

    Add solana support in animated qr

--- packages/animated-qr/src/AnimatedQRScanner/constant.ts
+++ packages/animated-qr/src/AnimatedQRScanner/constant.ts
@@ -1,6 +1,6 @@
 import { Purpose } from "./types";
 
 export const purposeToURType: Record<Purpose, string[]> = {
-  [Purpose.SYNC]: ["crypto-hdkey", "crypto-account"],
-  [Purpose.SIGN]: ["eth-signature"],
+  [Purpose.SYNC]: ["crypto-hdkey", "crypto-account", "crypto-multi-accounts"],
+  [Purpose.SIGN]: ["eth-signature", "sol-signature"],
 };
