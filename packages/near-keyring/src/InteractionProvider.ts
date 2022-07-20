8f1683a3b6991e7d33a4d4eb7eb789725f726526
mrzhao <zhaomengru2018@gmail.com>
2022-07-20 13:52:34 +0800

    feat: fix crypto multi account import

--- packages/near-keyring/src/InteractionProvider.ts
+++ packages/near-keyring/src/InteractionProvider.ts
@@ -1,9 +1,9 @@
 import {
-  CryptoMultiAccounts,
   NearSignature,
   NearSignRequest,
 } from "@keystonehq/bc-ur-registry-near";
 
+import { CryptoMultiAccounts } from "@keystonehq/bc-ur-registry"
 export interface InteractionProvider {
   readCryptoMultiAccounts: () => Promise<CryptoMultiAccounts>;
   requestSignature: (
