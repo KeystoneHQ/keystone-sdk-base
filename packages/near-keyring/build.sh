8bf776098b0b7c819784f773a09898e288f5e851
mrzhao <zhaomengru2018@gmail.com>
2022-08-03 16:03:50 +0800

    fix: update build script

--- packages/near-keyring/build.sh
+++ packages/near-keyring/build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
 cd $(dirname $0)/
 
-if [ ! -f "src/Secret.ts" ]; then
-    echo "src/Secret.ts does not exist!"
+if [ "$NODE_ENV" != "test" ]; then
+  if grep -Fq "SHOULD_BE_REPLACED" src/Secret.ts; then
+    echo -e "\nCredentials in src/Secret.ts should be replaced with real one.\n"
+    exit 1
+  fi
 fi
-
 tsdx build
