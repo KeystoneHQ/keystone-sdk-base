e69997c53913ba3b9460e285f29d344f85ca9541
aaronisme <aarondongchen@gmail.com>
2023-04-27 16:43:50 +0800

    feat: add the ur test

--- packages/ur-registry-eth/__tests__/EthSignRequest.test.ts
+++ packages/ur-registry-eth/__tests__/EthSignRequest.test.ts
@@ -3,6 +3,8 @@
 import { EthSignRequest, DataType } from "../src";
 import { CryptoKeypath, PathComponent } from "../src";
 import * as uuid from "uuid";
+import { URRegistryDecoder } from "@keystonehq/ur-decoder"
+import { RLP } from '@ethereumjs/rlp'
 
 describe("eth-sign-request", () => {
   it("test should genereate eth-sign-reqeust", () => {
@@ -53,25 +55,51 @@ describe("eth-sign-request", () => {
   });
 
   it("should construct an ethSignRequest object from string", () => {
-    const hdPath = "M/44'/1'/1'/0/1";
-    const xfp = "12345678";
-    const rlpData = Buffer.from(
-      "f849808609184e72a00082271094000000000000000000000000000000000000000080a47f7465737432000000000000000000000000000000000000000000000000000000600057808080",
-      "hex"
-    );
-    const ethRequestId = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
+    // const hdPath = "M/44'/1'/1'/0/1";
+    // const xfp = "12345678";
+    // const rlpData = Buffer.from(
+    //   "f849808609184e72a00082271094000000000000000000000000000000000000000080a47f7465737432000000000000000000000000000000000000000000000000000000600057808080",
+    //   "hex"
+    // );
+    // const ethRequestId = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
 
-    const ethRequest = EthSignRequest.constructETHRequest(
-      rlpData,
-      DataType.transaction,
-      hdPath,
-      xfp,
-      ethRequestId,
-      1
-    );
-    const ur = ethRequest.toUREncoder(1000).nextPart();
-    expect(ur).toBe(
-      "ur:eth-sign-request/onadtpdagdndcawmgtfrkigrpmndutdnbtkgfssbjnaohdgryagalalnascsgljpnbaelfdibemwaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaelaoxlbjyihjkjyeyaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaehnaehglalalaaxadaaadahtaaddyoeadlecsdwykadykadykaewkadwkaocybgeehfkswdtklffd"
-    );
+    // const ethRequest = EthSignRequest.constructETHRequest(
+    //   rlpData,
+    //   DataType.transaction,
+    //   hdPath,
+    //   xfp,
+    //   ethRequestId,
+    //   1
+    // );
+
+    const ur = "UR:ETH-SIGN-REQUEST/ONADTPDAGDDAKBPRKOKKYKFWGARFSFPLZMRHSNLDECAOHDDAVEHPLPAOGHBDVEAELFGMAYMWBNGHZTSNDMETGRGRRPWZVEAHRSHHRFBZNBCHPKZOLALAAALALAAXADAAAAAHTAADDYOEADLECSDWYKCSFNYKAEYKAEWKAEWKAOCYJOKBWEJZONWNHFT"
+
+    const decoder = new URRegistryDecoder();
+    decoder.receivePart(ur);
+    const result = decoder.resultUR();
+    console.log(result.cbor);
+    const ethSig = EthSignRequest.fromCBOR(result.cbor)
+
+
+    console.log(ethSig.getRequestId())
+    console.log(ethSig.getSignData().toString('hex'))
+    const serialized = ethSig.getSignData().toString('hex')
+    const buffer_se = Buffer.from(serialized, 'hex')
+    console.log(ethSig.getDataType())
+    console.log(ethSig.getChainId())
+    console.log(ethSig.getDerivationPath())
+    console.log(ethSig.getSourceFingerprint())
+    console.log(ethSig.getSignRequestAddress())
+    console.log(ethSig.getOrigin())
+
+
+    const a = RLP.decode(buffer_se)
+    console.log(a);
+    
+
+    // const ur = ethRequest.toUREncoder(1000).nextPart();
+    // expect(ur).toBe(
+    //   "ur:eth-sign-request/onadtpdagdndcawmgtfrkigrpmndutdnbtkgfssbjnaohdgryagalalnascsgljpnbaelfdibemwaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaelaoxlbjyihjkjyeyaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaeaehnaehglalalaaxadaaadahtaaddyoeadlecsdwykadykadykaewkadwkaocybgeehfkswdtklffd"
+    // );
   });
 });
