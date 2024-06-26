c52b74df163bb1df6845a3e69988a5d7013a0d65
Charon-Fan <Charon.Dian@gmail.com>
2024-06-26 10:01:01 +0800

    chore: update Cardano registry with new sign data request and signature types

---
+++ packages/ur-registry-cardano/src/CardanoSignDataRequest.ts
@@ -0,0 +1,123 @@
+import {
+  CryptoKeypath,
+  extend,
+  DataItem,
+  PathComponent,
+  RegistryItem,
+  DataItemMap,
+} from "@keystonehq/bc-ur-registry";
+import { ExtendedRegistryTypes } from "./RegistryType";
+import * as uuid from "uuid";
+
+const { decodeToDataItem, RegistryTypes } = extend;
+
+enum Keys {
+  requestId = 1,
+  signData,
+  derivationPath,
+  origin,
+  signType,
+}
+
+type signRequestProps = {
+  requestId?: Buffer;
+  signData: Buffer;
+  derivationPath: CryptoKeypath;
+  origin?: string;
+};
+
+export class CardanoSignDataRequest extends RegistryItem {
+  private requestId?: Buffer;
+  private signData: Buffer;
+  private derivationPath: CryptoKeypath;
+  private origin?: string;
+
+  getRegistryType = () => ExtendedRegistryTypes.CARDANO_SIGN_DATA_REQUEST;
+
+  constructor(args: signRequestProps) {
+    super();
+    this.requestId = args.requestId;
+    this.signData = args.signData;
+    this.derivationPath = args.derivationPath;
+    this.origin = args.origin;
+  }
+
+  public getRequestId = () => this.requestId;
+  public getSignData = () => this.signData;
+  public getDerivationPath = () => this.derivationPath.getPath();
+  public getOrigin = () => this.origin;
+
+  public toDataItem = () => {
+    const map: DataItemMap = {};
+    if (this.requestId) {
+      map[Keys.requestId] = new DataItem(
+        this.requestId,
+        RegistryTypes.UUID.getTag()
+      );
+    }
+
+    if (this.origin) {
+      map[Keys.origin] = this.origin;
+    }
+
+    map[Keys.signData] = this.signData;
+
+    const keyPath = this.derivationPath.toDataItem();
+    keyPath.setTag(this.derivationPath.getRegistryType().getTag());
+    map[Keys.derivationPath] = keyPath;
+
+    return new DataItem(map);
+  };
+
+  public static fromDataItem = (dataItem: DataItem) => {
+    const map = dataItem.getData();
+    const signData = map[Keys.signData];
+    const derivationPath = CryptoKeypath.fromDataItem(map[Keys.derivationPath]);
+    const requestId = map[Keys.requestId]
+      ? map[Keys.requestId].getData()
+      : undefined;
+    const origin = map[Keys.origin] ? map[Keys.origin] : undefined;
+
+    return new CardanoSignDataRequest({
+      requestId,
+      signData,
+      derivationPath,
+      origin,
+    });
+  };
+
+  public static fromCBOR = (_cborPayload: Buffer) => {
+    const dataItem = decodeToDataItem(_cborPayload);
+    return CardanoSignDataRequest.fromDataItem(dataItem);
+  };
+
+  public static constructCardanoSignDataRequest(
+    signData: string,
+    hdPath: string,
+    xfp: string,
+    uuidString?: string,
+    origin?: string
+  ) {
+    const paths = hdPath.replace(/[m|M]\//, "").split("/");
+    const hdpathObject = new CryptoKeypath(
+      paths.map((path) => {
+        const index = parseInt(path.replace("'", ""));
+        let isHardened = false;
+        if (path.endsWith("'")) {
+          isHardened = true;
+        }
+        return new PathComponent({ index, hardened: isHardened });
+      }),
+      Buffer.from(xfp, "hex")
+    );
+
+    return new CardanoSignDataRequest({
+      requestId: uuidString
+        ? Buffer.from(uuid.parse(uuidString) as Uint8Array)
+        : undefined,
+      signData: Buffer.from(signData, "hex"),
+      derivationPath: hdpathObject,
+      origin: origin || undefined,
+    });
+  }
+}
