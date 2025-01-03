fdb82b3c65eff6a5a9b640336c8cd06881df2876
Charon-Fan <Charon.Dian@gmail.com>
2025-01-03 17:36:02 +0800

    feat: enhance AvalancheSignRequest to include mfp, xpub, and walletIndex in request construction

--- packages/ur-registry-avalanche/src/AvalancheSignRequest.ts
+++ packages/ur-registry-avalanche/src/AvalancheSignRequest.ts
@@ -1,16 +1,37 @@
-import { DataItem, RegistryItem } from "@keystonehq/bc-ur-registry";
+import {
+  DataItem,
+  RegistryItem,
+  extend,
+  DataItemMap,
+} from "@keystonehq/bc-ur-registry";
 import { ExtendedRegistryTypes } from "./RegistryType";
 import * as uuid from "uuid";
 import { Buffer } from "buffer";
 
+const { RegistryTypes } = extend;
+
 type signRequestProps = {
   requestId?: Buffer;
   data: Buffer;
+  mfp: Buffer;
+  xpub: string;
+  walletIndex: number;
 };
+
+enum Keys {
+  requestId = 1,
+  signData = 2,
+  mfp = 3,
+  xpub = 6,
+  walletIndex = 7,
+}
 
 export class AvalancheSignRequest extends RegistryItem {
   private requestId?: Buffer;
   private data: Buffer;
+  private mfp: Buffer;
+  private xpub: string;
+  private walletIndex: number;
 
   getRegistryType = () => ExtendedRegistryTypes.AVALANCHE_SIGN_REQUEST;
 
@@ -18,21 +39,45 @@ export class AvalancheSignRequest extends RegistryItem {
     super();
     this.requestId = args.requestId;
     this.data = args.data;
+    this.mfp = args.mfp;
+    this.xpub = args.xpub;
+    this.walletIndex = args.walletIndex;
   }
 
   public getRequestId = () => this.requestId;
   public getSignData = () => this.data;
 
   public toDataItem = () => {
-    return new DataItem(this.data);
+    const map: DataItemMap = {};
+    if (this.requestId) {
+      map[Keys.requestId] = new DataItem(
+        this.requestId,
+        RegistryTypes.UUID.getTag()
+      );
+    }
+
+    map[Keys.signData] = this.data;
+    map[Keys.mfp] = this.mfp.readUInt32BE(0);
+    map[Keys.xpub] = this.xpub;
+    map[Keys.walletIndex] = Number(this.walletIndex);
+
+    return new DataItem(map);
   };
 
-  public static constructAvalancheRequest(data: Buffer, uuidString?: string) {
+  public static constructAvalancheRequest(
+    data: Buffer,
+    mfp: string,
+    xpub: string,
+    walletIndex: number
+  ) {
+    const uuidString = uuid.v4();
+
     return new AvalancheSignRequest({
       data,
-      requestId: uuidString
-        ? Buffer.from(uuid.parse(uuidString) as Uint8Array)
-        : undefined,
+      requestId: Buffer.from(uuid.parse(uuidString) as Uint8Array),
+      mfp: Buffer.from(mfp),
+      xpub,
+      walletIndex,
     });
   }
 }
