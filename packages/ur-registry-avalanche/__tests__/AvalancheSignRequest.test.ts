eadf558af00257dbf17bb7872d5b04c40c150d13
qkin <qkin-git@keyst.one>
2026-01-09 13:54:06 +0800

    feat: update AvalancheSignRequest test

--- packages/ur-registry-avalanche/__tests__/AvalancheSignRequest.test.ts
+++ packages/ur-registry-avalanche/__tests__/AvalancheSignRequest.test.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
 
 import { AvalancheSignRequest, AvalancheSignature } from "../src";
+import { UREncoder } from "@ngraveio/bc-ur";
 
 describe("avalanche-sign-request", () => {
   it("test should generate avalanche-sign-reqeust", () => {
@@ -8,7 +9,7 @@ describe("avalanche-sign-request", () => {
       "0000000000220000000100000000000000000000000000000000000000000000000000000000000000000000000221e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff0000000700000000006c2b4400000000000000000000000100000001b5e66be5c7093d1114d74940333c0c45f81092c521e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff00000007000000000176bbb400000000000000000000000100000001b5e66be5c7093d1114d74940333c0c45f81092c500000001cd53226620f4be2b6f6e43b1470f4f715b3bc7f40c4530cd5237a1c4156b537f0000000021e67317cbc4be2aeb00677ad6462778a8f52274b9d605df2591b23027a87dff000000050000000001e2fb33000000010000000000000000",
       "hex"
     );
-    const derivationPath = "m/44'/9000'/0'/0/0";
+    const derivationPath = "m/44'/9000'/0'/0/1";
     const utxos = [];
     const xfp = '2d0bdabc'
 
@@ -19,6 +20,10 @@ describe("avalanche-sign-request", () => {
       xfp
     );
     console.log('-----avalancheSignRequest--------',avalancheSignRequest.toCBOR().toString('hex'))
+    const ur = avalancheSignRequest.toUR();
+    const encoder = new UREncoder(ur, Infinity);
+    console.log('----- Full UR String -----');
+    console.log(encoder.nextPart().toUpperCase());
 
     const request = AvalancheSignRequest.fromDataItem(
       avalancheSignRequest.toDataItem()
@@ -26,7 +31,7 @@ describe("avalanche-sign-request", () => {
 
     const path = request.getDerivationPath();
     expect(path).toBeDefined();
-    expect(path.getPath()).toBe("44'/9000'/0'/0/0");
+    expect(path.getPath()).toBe("44'/9000'/0'/0/1");
 
     const recoveredXfp = request.getDerivationPath().getSourceFingerprint().toString('hex');
     expect(recoveredXfp).toBe(xfp)
